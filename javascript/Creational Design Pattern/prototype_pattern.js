17de9f34f6f8e17b807f4d9e26673619873f4745
hanpy <hanpy@deepbrainai.io>
2023-03-13 09:17:12 +0900

    prototype_pattern init settings

---
+++ javascript/Creational Design Pattern/prototype_pattern.js
@@ -0,0 +1,24 @@
+const macBook = {
+  color: "silver",
+  turnOn() {
+    console.log("turning on...");
+  },
+  turnOff() {
+    console.log("turning off...");
+  },
+};
+
+// Proper prototype cloning
+const myComputer = Object.create(macBook, { owner: { value: "Tim" } });
+console.log(myComputer.__proto__ === macBook);
+
+// Not a prototype copy
+const newComputer = { ...macBook, owner: "John" };
+console.log(newComputer.__proto__ === macBook);
+
+macBook.power = "USB-C";
+
+// The protoype gets the new value for 'power'
+console.log(myComputer.power);
+// But the non-prototype doesn't
+console.log(newComputer.power);
